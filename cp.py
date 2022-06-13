ed83e01b190e190df36aebac32eff2995f89a25c
Farmer-from-Space <nofx2002@gmail.com>
2022-06-13 17:14:29 +0900

    clear

--- cp.py
+++ cp.py
@@ -1,7 +1,6 @@
 #%%
 def solution(brown, yellow):
     temp=[]
-    temp2=[]
     answer = []
     total = brown + yellow
     for i in range(3, total):
@@ -9,16 +8,10 @@ def solution(brown, yellow):
            temp.append(i)
     temp.sort(reverse=True)
     for i in temp:
-        if total/i in temp:
-            temp2.append(i)
-    if len(temp2) == 1:
-            answer.append(temp2[0])
-            answer.append(temp2[0])
-    elif len(temp2) > 2:
-           answer.append(temp2[int(len(temp2)/2)-1])
-           answer.append(temp2[int(len(temp2)/2)])
-    else:
-        answer = temp2
+        if total/i in temp and (total/i-2)*(i-2) == yellow :
+            answer.append(i)
+            answer.append(total//i)
+            break
     return answer
 
 solution(22, 8)
--- sscg.py
+++ sscg.py
@@ -1,26 +1,34 @@
-def solution(numbers):
-    answer = 0
-
-    split_nums = [i for i in numbers]
-    # 숫자들의 모든 조합을 만들어서 하나씩 아래 num에 포문으로 넣어주면 될듯
-    # 근데 조합을 못만들겠다
-
-
     # 에라토스테네스의 체 방식으로 소수를 구하는 법(이해못함)
     # https://blog.naver.com/nkj2001/222658000054
 
-    prime_lists = [False, False] + [True] * (num - 1)
 
-    primes = []
+#%%
+def solution(numbers):
+    answer = 0
+    all_nums = []
+    from itertools import permutations
+    for i in range(len(numbers)):
+        permute = list(permutations(numbers,i+1))
+        for j in range(len(permute)):        
+            all_nums.append(int("".join(permute[j])))
+    
+    all_nums = set(all_nums)
+    
 
-    for i in range(2, num + 1):
-        if prime_lists[i]:
-            for j in range(2*i, num+1, i):
-                prime_lists[j] = False
-    primes = [i for i in range(2, num+1) if prime_lists[i] == True]
+    for num in all_nums:
+        if num != 0 and num != 1:
+            prime_lists = [False, False] + [True] * (num - 1)
 
-    if num in primes:
-        answer += 1
+            primes = []
+
+            for i in range(2, num + 1):
+                if prime_lists[i]:
+                    for j in range(2*i, num+1, i):
+                        prime_lists[j] = False
+            primes = [i for i in range(2, num+1) if prime_lists[i] == True]
+            
+            if i in primes:
+                answer += 1
 
     return answer
 # %%
