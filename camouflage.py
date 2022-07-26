17471d96fe7fe9e764a6d2c4f68f2b2bf24861f1
Farmer-from-Space <nofx2002@gmail.com>
2022-07-26 13:39:08 +0900

    learning

--- camouflage.py
+++ camouflage.py
@@ -3,12 +3,12 @@
 def solution(clothes):
     answer = 1
     
-    types = [y for x, y in clothes]
-    counts = [types.count(t) for t in set(types)]
+    types = [y for x, y in clothes] # 의상의 타입들만 가져온다
+    counts = [types.count(t) for t in set(types)] # 중복을 제거하고 타입별 몇개씩있는지 구하고, 곱해서 경우의 수를 구할예정
     for i in counts:
-        answer *= i + 1
+        answer *= i + 1 # 해당 타입의 의상을 아예 안입을 경우도 경우의 수에 추가해서 곱해준다
     
-    return answer - 1
+    return answer - 1 # 아무것도 안입은 경우의 수도 포함된 상태이므로 1을 빼준다
 
 #%%
 
