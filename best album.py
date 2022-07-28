9a57d9563ac078a698ff5e25a39f280f4e09a87e
Farmer-from-Space <nofx2002@gmail.com>
2022-07-28 17:58:53 +0900

    studying

---
+++ best album.py
@@ -0,0 +1,30 @@
+#%%
+# https://velog.io/@sem/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-LEVEL3-%EB%B2%A0%EC%8A%A4%ED%8A%B8%EC%95%A8%EB%B2%94-Python
+'''많이 배웠습니다 진짜...'''
+# %%
+def solution(genres, plays):
+    answer = []
+
+    dic1 = {}
+    dic2 = {}
+
+    for i, (g, p) in enumerate(zip(genres, plays)):
+        if g not in dic1:
+            dic1[g] = [(i, p)]
+        else:
+            dic1[g].append((i, p))
+
+        if g not in dic2:
+            dic2[g] = p
+        else:
+            dic2[g] += p
+
+    for (k, v) in sorted(dic2.items(), key=lambda x:x[1], reverse=True):
+        for (i, p) in sorted(dic1[k], key=lambda x:x[1], reverse=True)[:2]:
+            answer.append(i)
+
+    return answer
+
+#%%
+solution(["classic", "pop", "classic", "classic", "pop"], [500, 600, 150, 800, 2500])
+# %%
